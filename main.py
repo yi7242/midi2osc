7d16b73082c8e57a84c1c008f5d42d0f52687080
My Name <47847960+yi7242@users.noreply.github.com>
2024-01-14 22:27:56 +0900

    aaa

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import mido 
-from mido import Message
-msg = Message('note_on', note=60)
-ports = mido.get_output_names()
-print(ports)
-print(msg)
+import time
+import rtmidi
+
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+
+if available_ports:
+    midiout.open_port(0)
+else:
+    midiout.open_virtual_port("My virtual output")
+
+with midiout:
+    note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
+    note_off = [0x80, 60, 0]
+    midiout.send_message(note_on)
+    time.sleep(0.5)
+    midiout.send_message(note_off)
+    time.sleep(0.1)
+
+del midiout
